2038f0a4ef865a6175cf7bac27e0192402f6c950
Inaf Suwanto <inafsuwanto@dinsecta.com>
2016-04-23 12:44:40 +0700

    App
    App Android Surya Kencana

---
+++ app/src/test/java/com/suryakencana/sikscloud/SessionManagerTest.java
@@ -0,0 +1,14 @@
+package com.suryakencana.sikscloud;
+
+import org.junit.Test;
+
+/**
+ * Created by INAF on 23/04/2016.
+ */
+public class SessionManagerTest {
+
+    @Test
+    public void testGetUserDetails() throws Exception {
+
+    }
+}
